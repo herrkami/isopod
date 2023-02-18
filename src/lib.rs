b2585a0b942bf44cbf62740dc2078d83ae07e333
herrkami <aebsor@gmail.com>
2023-02-18 11:49:33 +0100

    Initial commit with wave table generators

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod env;
+pub mod fx;
+pub mod osc;
+pub mod synth;
---
+++ src/osc/luts/mod.rs
@@ -0,0 +1,7 @@
+// Includes all wavetable files
+include!("sine_i8.rs");
+include!("sine_i16.rs");
+include!("sine_i32.rs");
+include!("exp_i8.rs");
+include!("exp_i16.rs");
+include!("exp_i32.rs");
