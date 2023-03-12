1e301f79ace2e6ef4a86f81b2d95b27f809d6dff
herrkami <aebsor@gmail.com>
2023-03-12 18:39:04 +0100

    Add working StateVariableFilter

--- src/fx/filter.rs
+++ src/fx/filter.rs
@@ -1,12 +1,12 @@
 use crate::util::units::mHz;
 
 // Constant multiplier for precision divisions
-const DIVIDER: u16 = 2048;
-const Q_MAX: u16 = DIVIDER;
+const NORM: u32 = 1 << 12;
+pub const Q_MAX: u32 = NORM;
 
-// CF = 1/(2*pi*dt) for cHz
+// MF = 1/(2*pi*dt) for mHz
 // sample rate dt = 60 u
-const CF: i32 = 265258;
+// const MF: i32 = 2652582;
 
 pub struct StateVariableFilter {
     lp: i16,
@@ -15,7 +15,7 @@ pub struct StateVariableFilter {
     no: i16,
 
     ft: u32,
-    q_inv: u16,
+    q_inv: u32,
 
     msample_rate: mHz,
 }
@@ -35,16 +35,19 @@ impl StateVariableFilter {
             no: 0,
 
             ft: 0,
-            q_inv: DIVIDER - 0,
+            q_inv: NORM - 0,
 
             msample_rate: mHz(44_100_000),
         }
     }
 
     pub fn feed(&mut self, signal: i16) {
-        self.lp += ((self.ft as i32 * self.bp as i32) / DIVIDER as i32) as i16;
-        self.hp = signal - self.lp - (self.q_inv as i16 * self.bp) / DIVIDER as i16;
-        self.bp += ((self.ft as i32 * self.hp as i32) / DIVIDER as i32) as i16;
+        // TODO To gain performance we might try to store the sign, perform u32
+        // divisions, then restore the sign.
+        self.lp += ((self.ft as i32 * self.bp as i32) / NORM as i32) as i16;
+        // dbg!(signal, self.lp, self.bp, self.hp, NORM, self.q_inv);
+        self.hp = signal - self.lp - ((self.q_inv as i32 * self.bp as i32) / NORM as i32) as i16;
+        self.bp += ((self.ft as i32 * self.hp as i32) / NORM as i32) as i16;
         self.no = self.hp + self.lp;
     }
 
@@ -67,14 +70,18 @@ impl StateVariableFilter {
     pub fn set_mfreq(&mut self, mfreq: mHz) {
         // We use a first order Tailor approximation here. -> Deviations close
         // to Nyquist frequency.
-        self.ft = ((DIVIDER as i32 * mfreq.0 as i32) / self.msample_rate.0 as i32) as u32;
+        self.ft = ((NORM as i64 * mfreq.0 as i64) / self.msample_rate.0 as i64) as u32;
     }
 
-    pub fn set_q(&mut self, q: u16) {
-        if q > DIVIDER as u16 {
+    pub fn set_q(&mut self, q: u32) {
+        if q > NORM {
             self.q_inv = 0;
         } else {
-            self.q_inv = DIVIDER as u16 - q;
+            self.q_inv = NORM - q;
         }
+    }
+
+    pub fn set_msample_rate(&mut self, msample_rate: mHz) {
+        self.msample_rate = msample_rate;
     }
 }
