ef7a3e2ec69d16b74eb13b6bf08eab1b941d80ac
herrkami <aebsor@gmail.com>
2023-03-12 18:38:32 +0100

    Improve Synth trait

--- src/synth/mod.rs
+++ src/synth/mod.rs
@@ -1,8 +1,9 @@
-trait Synth<T> {
-    fn init(&mut self);
-    fn _next(&mut self) -> Option<T>;
+use crate::util::units::Hz;
 
-    fn set_sample_rate(&mut self, sample_rate: u32);
-    fn get_sample_rate(&self) -> u32;
-    fn is_running(&self) -> bool;
+pub trait Synth {
+    fn new() -> Self;
+    fn _next(&mut self) -> Option<i16>;
+
+    fn get_sample_rate(&self) -> Hz;
+    fn set_sample_rate(&mut self, sample_rate: Hz);
 }
